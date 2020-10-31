937eb77c856027dba254a8d9092b16212c121a98
Mihael Zlatev <mihael.zlatev@sap.com>
2020-10-31 15:39:00 +0200

    Update 3.scm

--- 3.scm
+++ 3.scm
@@ -6,7 +6,23 @@
          #t))
   )
 
+(define (add s c)
+  (if (string? c)
+      (string-append s c)
+      (string-append s (make-string 1 c)))
+  )
 
+(define (isEmpty s)
+  (= (string-length s) 0)
+  )
+
+(define (top stack)
+  (string-ref stack (- (string-length stack) 1))
+  )
+
+(define (pop stack)
+  (substring stack 0 (- (string-length stack) 1))
+  )
 
 (define (removeWhiteSpacesHelper s a b acc)
   (cond((= (+ a 1) b)
@@ -41,4 +57,47 @@
 (define (removeUselessWhiteSpaces s)
   (define x (removeWhiteSpacesHelper s 0 (string-length s) ""))
   (removeWhiteSpacesHelper2 x 0 (string-length x) "")
+  )
+
+(define (prec operator)
+  (cond ((eq? operator #\^)
+         3)
+        ((or (eq? operator #\*) (eq? operator #\/))
+         2)
+        ((or (eq? operator #\+) (eq? operator #\-))
+         1)
+        (else
+         0))
+  )
+
+(define (isOperator operator)
+  (or (eq? operator #\-)(or (or (eq? operator #\/) (eq? operator #\+)) (or (eq? operator #\^) (eq? operator #\*))))
+  )
+
+(define (emptyStackInResult stack result)
+  (cond ((isEmpty stack)
+         result)
+        (else
+         (emptyStackInResult (pop stack) (add result (top stack)))))
+  )
+
+(define (infixToPostfixHelper s a b stack result )
+  (define (predicate s a stack)
+    (and (not (isEmpty stack)) ( <= (prec (string-ref s a)) (prec (top stack))))
+    )
+  (cond ((= a b)
+          (emptyStackInResult stack result))
+        (else
+         (cond ((isOperator (string-ref s a))
+                (cond ((not (predicate s a stack))
+                       (infixToPostfixHelper s (+ a 1) b (add stack (string-ref s a)) (add result #\,)))
+                      (else
+                       (infixToPostfixHelper s a b (pop stack) (add result (top stack))))))
+               (else
+                (infixToPostfixHelper s (+ a 1) b stack (add result (string-ref s a)))))))
+  )
+
+(define (infixToPostfix s)
+  (define exp (removeUselessWhiteSpaces s))
+  (infixToPostfixHelper exp 0 (string-length exp) "" "")
   )
