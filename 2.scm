39929fc4799e6e5a161abae564260fccd37bbd76
Mihael Zlatev <mihael.zlatev@sap.com>
2020-11-05 00:40:24 +0200

    Update 2.scm

--- 2.scm
+++ 2.scm
@@ -24,6 +24,24 @@
   (string-append (substring s 0 (- (string-length s) (+ n 1))) symbol (substring s (- (string-length s) n)))
   )
 
+(define (string-to-decimal set)
+  (binary-to-decimal (string->number set))
+  )
+
+(define (set-operation-helper set1 set2 a result p)
+    (define set1-in-decimal (string-to-decimal set1))
+    (define set2-in-decimal (string-to-decimal set2))
+    (define length (if (> (string-length set1) (string-length set2))
+                     (string-length set1)
+                     (string-length set2)))
+    (cond ((= a length)
+           result)
+          ((p a)
+           (set-operation-helper set1 set2 (+ a 1) (set-add result a) p))
+          (else
+           (set-operation-helper set1 set2 (+ a 1) result p)))
+    )
+
 (define (set-contains? set element)
   (define s (number->string set 2))
   (define pos (+ element 1))
@@ -62,24 +80,9 @@
                                                              0
                                                           )) 0)
   )
-
-(define (string-to-decimal set)
-  (binary-to-decimal (string->number set))
+(define (set-empty? set)
+  (= (set-size set) 0)
   )
-
-(define (set-operation-helper set1 set2 a result p)
-    (define set1-in-decimal (string-to-decimal set1))
-    (define set2-in-decimal (string-to-decimal set2))
-    (define length (if (> (string-length set1) (string-length set2))
-                     (string-length set1)
-                     (string-length set2)))
-    (cond ((= a length)
-           result)
-          ((p a)
-           (set-operation-helper set1 set2 (+ a 1) (set-add result a) p))
-          (else
-           (set-operation-helper set1 set2 (+ a 1) result p)))
-    )
 
 (define (set-union set1 set2)
   (define set1-string (number->string set1 2))
