308c9662b3022c8a8d719b8a3cd42283f1cb8e1d
mishozz <mzlatev345@gmail.com>
2021-01-14 11:22:17 +0200

    Update Task2_Test.hs

--- Task2_Test.hs
+++ Task2_Test.hs
@@ -1,4 +1,4 @@
-import Task2 (grayscale,floodFill,Rgb(Rgb),Image(Image))
+import Task2 (grayscale,imgToP3Format,floodFill,toCorrectContent,transfromToWord8,Rgb(Rgb),Image(Image))
 import Test.HUnit
 
 inputImage1x1 :: Image
@@ -50,6 +50,30 @@ expectedFloodFill9x9 = [[Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 1
                         [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128],
                         [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128]]
 
+rgb10x10 :: [[Rgb]]
+rgb10x10 = [[Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 100 100 100, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128,Rgb 128 128 128],
+                        [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 100 100 100,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128,Rgb 128 128 128],
+                        [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 100 100 100,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128,Rgb 128 128 128],
+                        [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 100 100 100,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128,Rgb 128 128 128],
+                        [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 100 100 100,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128,Rgb 128 128 128],
+                        [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 100 100 100,Rgb 100 100 100, Rgb 100 100 100, Rgb 100 100 100,Rgb 100 100 100,Rgb 100 100 100],
+                        [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128,Rgb 128 128 128],
+                        [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128,Rgb 128 128 128],
+                        [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128,Rgb 128 128 128],
+                        [Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128,Rgb 128 128 128]]
+
+expectedRgb10x10 :: [[Rgb]]
+expectedRgb10x10 = [[Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0, Rgb 255 0 0,Rgb 100 100 100, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128,Rgb 128 128 128],
+                        [Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0, Rgb 100 100 100, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128],
+                        [Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0, Rgb 100 100 100, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128],
+                        [Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0, Rgb 100 100 100, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128],
+                        [Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0, Rgb 100 100 100, Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128, Rgb 128 128 128, Rgb 128 128 128],
+                        [Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0, Rgb 100 100 100,Rgb 100 100 100, Rgb 100 100 100,Rgb 100 100 100, Rgb 100 100 100, Rgb 100 100 100],
+                        [Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0],
+                        [Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0],
+                        [Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0],
+                        [Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0, Rgb 255 0 0,Rgb 255 0 0]]
+
 emptyImage :: Image
 emptyImage = Image 0 0 [[]]
 
@@ -60,7 +84,7 @@ testGrayscaleSuccessful = TestCase $ do
     assertEqual "grayscale should return Image 1 1 [[Rgb 66 66 66]]" expectedGrayscaleImage1x1 (grayscale inputImage1x1)
     assertEqual "grayscale should return  Image 3 2 [[Rgb 76 76 76, Rgb 122 122 122, Rgb 227 227 227], [Rgb 150 150 150, Rgb 255 255 255, Rgb 203 203 203]]"
      expectedGrayscaleImage3x2 (grayscale inputImage3x2)
-    assertEqual "graysca;e should return Image 3 1 [[Rgb 76 76 76, Rgb 122 122 122, Rgb 227 227 227]]" expectedGrayscaleImage3x1 (grayscale inputImage3x1)
+    assertEqual "grayscale should return Image 3 1 [[Rgb 76 76 76, Rgb 122 122 122, Rgb 227 227 227]]" expectedGrayscaleImage3x1 (grayscale inputImage3x1)
 
 testFloodFillReturnsNewImage :: Test
 testFloodFillReturnsNewImage = TestCase $ do
@@ -69,6 +93,7 @@ testFloodFillReturnsNewImage = TestCase $ do
     assertEqual "floodFill should return Image 3 2 [[Rgb 128 128 128, Rgb 128 128 128,Rgb 128 128 128],[Rgb 255 255 255, Rgb 255 255 255, Rgb 255 255 255]]"
      (Image 3 2 expectedFloodFill3x2) (floodFill (Rgb 128 128 128) 0 1  (Image 3 2 rgb3x2))
     assertEqual "floodFill should fill the whole Content"  (Image 9 9 expectedFloodFill9x9) (floodFill (Rgb 128 128 128) 4 3 (Image 9 9 rgb9x9))
+    assertEqual "floodFill succesll Image 10 by 10" (Image 10 10 expectedRgb10x10) (floodFill (Rgb 255 0 0) 1 3 (Image 10 10 rgb10x10))
 
 testFloodFillReturnSameImage :: Test
 testFloodFillReturnSameImage = TestCase $ do
@@ -81,9 +106,38 @@ testFloodFillFail = TestCase $ do
     assertEqual  "floodFill should return error with negative cordinates" (error "Invalid cordinates x and y") (floodFill (Rgb 128 128 128) (-1) 10 (Image 9 9 rgb9x9))
     assertEqual  "floodFill should return error with Empty image" (error "Unable to floodFill empty Image content") (floodFill (Rgb 255 0 0) 0 0 emptyImage)
 
+testImgToP3Format :: Test
+testImgToP3Format = TestCase $ do
+    assertEqual "imgToP3 should return list in the form of [<format>,<w>,<h>,<max color>, <rgb>]" ["P3","3 2","255","255 0 0","155 128 0","255 255 0","0 255 0","255 255 255","128 255 128"]
+     (imgToP3Format inputImage3x2)
+    assertEqual "imgToP3 should return list in the form of [<format>,<w>,<h>,<max color>, <rgb>]" ["P3","3 1","255","255 0 0","155 128 0","255 255 0"]
+     (imgToP3Format inputImage3x1)
+
+testCorrectContent :: Test 
+testCorrectContent = TestCase $ do
+    assertEqual "toCorrectContent should return Image content 3 by 2"
+     [[Rgb 76 76 76, Rgb 76 76 76, Rgb 76 76 76],[Rgb 76 76 76, Rgb 76 76 76, Rgb 76 76 76]]
+      (toCorrectContent [[Rgb 76 76 76],[Rgb 76 76 76],[Rgb 76 76 76],[Rgb 76 76 76],[Rgb 76 76 76],[Rgb 76 76 76]] 3)
+    assertEqual "toCorrectContent should return Image content 1 by 1"  [[Rgb 76 76 76]] (toCorrectContent [[Rgb 76 76 76]] 1)
+    assertEqual "toCorrectContent should return Image content 3 by 3"
+     [[Rgb 255 0 0, Rgb 255 0 0, Rgb 255 0 0],[Rgb 255 255 255, Rgb 255 0 0, Rgb 255 255 255],[Rgb 255 0 0,Rgb 255 0 0,Rgb 255 0 0]]
+     (toCorrectContent [[Rgb 255 0 0], [Rgb 255 0 0], [Rgb 255 0 0],[Rgb 255 255 255], [Rgb 255 0 0], [Rgb 255 255 255],[Rgb 255 0 0],[Rgb 255 0 0,Rgb 255 0 0]] 3)
+    assertEqual "tocorrectContent should return empty content" [[]] (toCorrectContent [[]] 0)
+
+testTransfromToWord8 :: Test
+testTransfromToWord8 = TestCase $ do 
+    assertEqual "transfromToWord8 should return 255" 255 (transfromToWord8 (Rgb 255 255 255))
+    assertEqual "transfromToWord8 should return 128" 128 (transfromToWord8 (Rgb 128 128 128))
+    assertEqual "transfromToWord8 should return 0" 0 (transfromToWord8 (Rgb 0 0 0))
+    assertEqual "transfromToWord8 should return 227" 227 (transfromToWord8 (Rgb 255 255 0))
+    assertEqual "transfromToWord8 should return 227" 76 (transfromToWord8 (Rgb 255 0 0))
+
+
 
 tl :: Test
-tl =  TestList [testGrayscaleSuccessful, testFloodFillReturnsNewImage,testFloodFillReturnSameImage]
+tl =  TestList [testGrayscaleSuccessful, testFloodFillReturnsNewImage
+    ,testFloodFillReturnSameImage,testImgToP3Format,testCorrectContent,
+    testTransfromToWord8]
 
 
 main = runTestTT tl
